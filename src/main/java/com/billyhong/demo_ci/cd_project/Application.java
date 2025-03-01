2b96b4b250a82577f3b92381747b760e8afc0c99
Billy Hong <hongwk2001@yahoo.com>
2025-03-01 08:17:22 -0600

    init

---
+++ src/main/java/com/billyhong/demo_ci/cd_project/Application.java
@@ -0,0 +1,22 @@
+package com.billyhong.demo_ci.cd_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	@GetMapping("/welcome")
+	public String welcome(){
+		return "Welcome to class";
+	}
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/billyhong/demo_ci/cd_project/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.billyhong.demo_ci.cd_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
